3cfee8bf575b782e72e7555e2646729275b80d5b
CamilaFre <camila.frech@hotmail.com>
2026-03-16 20:19:12 -0300

    aprendendo funcoes

---
+++ dia_06/02_juros_compostos.py
@@ -0,0 +1,18 @@
+#%%
+
+def juros_compostos(anos:int) -> float:
+
+    """
+    juros compostos servem para calcular o retorno financeiro a partir de um aporte
+
+
+
+    """
+
+
+    return 1000*1.13**anos
+
+juros_compostos(4)
+
+
+# %%
